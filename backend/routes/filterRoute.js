ca309b48a13802eb47d6862260414acabb8eda36
lasanga890 <lasangad890@gmail.com>
2024-09-25 19:34:45 +0530

    colors update

--- backend/routes/filterRoute.js
+++ backend/routes/filterRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 import { getFilters } from "../controllers/filters/filterReciept.js";
+import { updateColorController } from "../controllers/filters/colorsOfRaw.js";
 
 const router = express.Router();
 
 router.get("/get-filter", getFilters);
+router.put("/update-color/:id", updateColorController);
 
 export default router;
