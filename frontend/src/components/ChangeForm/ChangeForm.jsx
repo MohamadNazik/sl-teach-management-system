04e1a645243f59134f7c443f9590231038f7058f
Nazik <shmnazik79@gmail.com>
2024-09-27 15:11:19 +0530

    Finished 1.0.0v

--- frontend/src/components/ChangeForm/ChangeForm.jsx
+++ frontend/src/components/ChangeForm/ChangeForm.jsx
@@ -100,12 +100,12 @@ const ChangeForm = () => {
   return (
     <section>
       <Header page="Change Form" isDashboard={false} />
-      <div className="w-[100] m-5 bg-white p-10 flex flex-col xl:flex-row items-start gap-8 xl:gap-40 rounded-xl">
+      <div className="w-[100] mt-12 sm:mt-5 m-5 bg-white p-10 flex flex-col xl:flex-row items-center sm:items-start gap-8 xl:gap-40 rounded-xl">
         <div className="flex flex-col gap-4">
           {inputFields.map((field, index) => (
             <div
               key={index}
-              className="flex gap-3 items-center w-[30rem] border-2 border-black rounded-md p-2"
+              className="flex gap-3 items-center w-[18rem] sm:w-[30rem] border-2 border-black rounded-md p-2"
             >
               <img
                 src={crossIcon}
@@ -119,7 +119,7 @@ const ChangeForm = () => {
             </div>
           ))}
         </div>
-        <div className="bg-white w-[25rem] xl:w-[40rem] p-10 flex flex-col items-center rounded-xl drop-shadow-[0_15px_35px_rgba(0,0,0,0.25)]">
+        <div className="bg-white w-[20rem] sm:w-[35rem] xl:w-[40rem] p-10 flex flex-col items-center rounded-xl drop-shadow-[0_15px_35px_rgba(0,0,0,0.25)]">
           <h3 className="text-lg font-medium mb-8">ADD NEW FIELD</h3>
           <form
             onSubmit={handleSubmit}
@@ -156,13 +156,13 @@ const ChangeForm = () => {
             <div className="flex gap-5">
               <button
                 type="submit"
-                className="text-md rounded-md w-40 xl:w-52 pl-6 pr-6 pt-2 pb-2 bg-[#BF3606] mt-8 font-semibold text-white"
+                className="text-md rounded-md w-32 sm:w-40 xl:w-52 pl-6 pr-6 pt-2 pb-2 bg-[#BF3606] mt-8 font-semibold text-white"
               >
                 Add
               </button>
               <button
                 type="button"
-                className="text-md rounded-md w-40 xl:w-52 pl-6 pr-6 pt-2 pb-2 bg-[#BF3606] mt-8 font-semibold text-white"
+                className="text-md rounded-md w-32 sm:w-40 xl:w-52 pl-6 pr-6 pt-2 pb-2 bg-[#BF3606] mt-8 font-semibold text-white"
                 onClick={() => document.querySelector("form").reset()} // Clears the form
               >
                 Clear
